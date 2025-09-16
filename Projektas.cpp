dd7ad612d9dde28c09d2cc498e0f4b53233d0e8c
Emilija0621 <emilija.adomonyte@gmail.com>
2025-09-16 23:32:15 +0300

    Update Projektas.cpp
    Naujos funkcijos skaiciuoti_vidurki, skaiciuoti_galutinis_pazymys,  gražesnė išvesties forma.

--- Projektas.cpp
+++ Projektas.cpp
@@ -12,52 +12,78 @@ using std::vector;
 using std::setw;
 using std::left;
 using std::right;
+using std::string;
+
 
 struct studentas{
     string vardas;
     string pavarde;
-    vector <int> pazimys;   // resizable array
-    int egzamino;
-    double galutinis;
+    vector<int> pazymiai;
+    int egzamino_pazymys;
+    double galutinis_pazymys;
 };
 
+
 studentas studentas_ivestis();
+double skaiciuoti_vidurki(const vector<int>& pazymiai);
+double skaiciuoti_galutinis_pazymys(const vector<int>& pazymiai, int egzaminas);
+
 
 int main() {
     vector <studentas> grupe;
-    cout << "Kiek studentu yra grupeje? ";
     int m;
-    cin >> m;
-    for (auto z = 0; z < m; z++) {
-        grupe.push_back(studentas_ivestis());     // prideda reiksme i gala vektoriaus
-    };
-    cout << "studento informacija: " << endl;
-    for (auto past: grupe) {
-        cout << setw(10) << left << past.vardas << "|" << setw(15) << right << past.pavarde << "|";
-        for (auto& a: past.pazimys) {
-            cout << setw(3) << a << "|";
-        };
-        cout << setw(5) << past.egzamino << "|" << setw(10) << past.galutinis << endl;
-    };
-}
-
-
-
-studentas studentas_ivestis() {
-    int n, laik_paz, sum = 0;
-    cout << "sveiki" << endl;
-    studentas pirmas;
-    cout << "iveskite stundento duomenis: " << endl;
-    cout << "vardas: "; cin >> pirmas.vardas;
-    cout << "pavarde: "; cin >> pirmas.pavarde;
-    cout << "kiek pazymiu turi? " << pirmas.vardas << " " << pirmas.pavarde << ": "; cin >> n;
-    for (int a = 0; a < n; a++) {
-        cout << a + 1 << ": "; cin >> laik_paz;
-        pirmas.pazimys.push_back(laik_paz);
-        sum += laik_paz;   // sum += pirmas.pazimys[a]
-    };
-    cout << "iveskite egzamino pazymi: "; cin >> pirmas.egzamino;
-    pirmas.galutinis = double (sum) / double (n) * 0.4 + pirmas.egzamino * 0.6;
-    return pirmas;
-}
+    cout << "Kiek studentu yra grupeje? "; cin >> m;
     
+    for (auto z=0; z<m; z++){
+        grupe.push_back(studentas_ivestis());
+    };
+    
+    for (auto &s : grupe) {
+        s.galutinis_pazymys = skaiciuoti_galutinis_pazymys(s.pazymiai, s.egzamino_pazymys);
+    };
+        
+    cout << "Studentu informacija: " << endl;
+    cout << setw(10) << left << "Vardas" << "|" << setw(15) << left << "Pavarde" << "|" << setw(5) << left << "Galutinis (Vid.) " << endl;
+    cout << string(46, '-') << endl;
+    
+    for (auto past: grupe){
+        cout << setw(10) << left << past.vardas << "|" << setw(15) << left << past.pavarde << "|";
+        cout << setw(10) << past.galutinis_pazymys << endl;
+    };
+};
+
+
+studentas studentas_ivestis(){
+    int n, laikinas_paz;
+    cout << "Sveiki!" << endl;
+    studentas pirmas;
+    cout << "Iveskite studento duomenis." << endl;
+    cout << "Vardas: "; cin >> pirmas.vardas;
+    cout << "Pavarde: "; cin >> pirmas.pavarde;
+    cout << "Kiek pazymiu turi " << pirmas.vardas << " " << pirmas.pavarde << "? "; cin >> n;
+    
+    for (int a = 0; a<n; a++){
+        cout << a + 1 << ": ";
+        cin >> laikinas_paz;
+        pirmas.pazymiai.push_back(laikinas_paz);
+    };
+    
+    cout << "Iveskite egzamino pazymi: "; cin >> pirmas.egzamino_pazymys;
+    return pirmas;
+};
+
+
+double skaiciuoti_vidurki(const vector<int>& pazymiai){
+    if (pazymiai.empty()) return 0;
+    double suma = 0;
+    for (auto paz: pazymiai){
+        suma += paz;
+    };
+    return suma / pazymiai.size();
+};
+
+
+double skaiciuoti_galutinis_pazymys(const vector<int>& pazymiai, int egzaminas){
+    double vidurkis = skaiciuoti_vidurki(pazymiai);
+    return 0.4 * vidurkis + 0.6 * egzaminas;
+};
