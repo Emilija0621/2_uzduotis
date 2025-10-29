3a4b6bf62e5d29bf234b190220a4b3c222c2913e
Emilija0621 <emilija.adomonyte@gmail.com>
2025-10-30 00:18:49 +0200

    Update failu_skaitymas.h

--- Užduotis list/failu_skaitymas.h
+++ Užduotis list/failu_skaitymas.h
@@ -1,8 +1,8 @@
 #include <string>
-#include <vector>
+#include <list>
 #include "studentas.h"
 
-using std::vector;
 using std::string;
+using std::list;
 
-void nuskaityti_duomenis_is_failo(const string& failo_pavadinimas, vector<studentas>& grupe);
+void nuskaityti_duomenis_is_failo(const string& failo_pavadinimas, list<studentas>& grupe);
