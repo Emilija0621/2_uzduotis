deb3ce63b5fef36a6cf081ce15939cf35d9c2765
Emilija0621 <emilija.adomonyte@gmail.com>
2025-10-30 00:18:14 +0200

    Update failu_generavimas.h

--- Užduotis list/failu_generavimas.h
+++ Užduotis list/failu_generavimas.h
@@ -1,14 +1,11 @@
-#include <string>
+#include<string>
+#include<list>
 #include "studentas.h"
-#include <vector>
 
-using std::vector;
+using std::list;
 using std::string;
 
 void sugeneruoti_faila(const string& failo_pavadinimas, int studentu_kiekis, int nd_pazymiu_kiekis);
 void failo_generavimo_pasirinkimas();
-void padalinti_ir_isvesti_studentus(vector<studentas>& grupe);
-void isvesti_padalintus_i_faila(const vector<studentas>& grupe, const string& failo_pavadinimas, int pagal_kuri_galutini);
-
-
-
+void padalinti_ir_isvesti_studentus(list<studentas>& grupe);
+void isvesti_padalintus_i_faila(const list<studentas>& grupe, const string& failo_pavadinimas, int pagal_kuri_galutini);
