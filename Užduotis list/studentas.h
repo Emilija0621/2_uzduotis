54d1f1c5b564fbdf09350a77068d9b58eec84a74
Emilija0621 <emilija.adomonyte@gmail.com>
2025-10-30 00:20:43 +0200

    Update studentas.h

--- Užduotis list/studentas.h
+++ Užduotis list/studentas.h
@@ -1,14 +1,14 @@
 #pragma once
-#include<vector>
+#include<list>
 #include<string>
 
 using std::string;
-using std::vector;
+using std::list;
 
 struct studentas{
     string vardas;
     string pavarde;
-    vector<int> pazymiai;
+    list<int> pazymiai;
     int egzamino_pazymys;
     double galutinis_vidurkis;
     double galutinis_mediana;
