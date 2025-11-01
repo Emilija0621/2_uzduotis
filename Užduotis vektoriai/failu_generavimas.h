f93c0b8e7ee5f29cc0c44533f094c9ac9fc1459d
Emilija0621 <emilija.adomonyte@gmail.com>
2025-11-01 18:12:57 +0200

    Update failu_generavimas.h

--- Užduotis vektoriai/failu_generavimas.h
+++ Užduotis vektoriai/failu_generavimas.h
@@ -9,6 +9,8 @@ void sugeneruoti_faila(const string& failo_pavadinimas, int studentu_kiekis, int
 void failo_generavimo_pasirinkimas();
 void padalinti_ir_isvesti_studentus(vector<studentas>& grupe);
 void isvesti_padalintus_i_faila(const vector<studentas>& grupe, const string& failo_pavadinimas, int pagal_kuri_galutini);
+void padalinimo_1_strategija(vector<studentas> &grupe, vector<studentas> &vargsiukai, vector<studentas> &kietiakai, int pagal_kuri_galutini);
+void padalinimo_2_strategija(vector<studentas> &grupe, vector<studentas> &vargsiukai, int pagal_kuri_galutini);
 
 
 
