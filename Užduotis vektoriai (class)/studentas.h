a4b8a96db7d0ce78aac979f18b1f1335d0a45388
Emilija0621 <emilija.adomonyte@gmail.com>
2025-11-06 02:02:10 +0200

    Update studentas.h

--- Užduotis vektoriai (class)/studentas.h
+++ Užduotis vektoriai (class)/studentas.h
@@ -18,28 +18,28 @@ private:
     
 public:
     Studentas() : egzamino_pazymys_(0), galutinis_vidurkis_(0), galutinis_mediana_(0){}
-    Studentas(istream& is);
+    Studentas(istream& is, bool atsitiktiniai_balai);
     inline string vardas() const {return vardas_;}
     inline string pavarde() const {return pavarde_;}
-    inline double galutinis_vidurkis() const {return galutinis_vidurkis_;}
-    inline double galutinis_mediana() const {return galutinis_mediana_;}
     inline double egzamino_pazymys() const {return egzamino_pazymys_;}
+    inline double galutinis_vidurkis() const {return galutinis_vidurkis_;}
+    inline double galutinis_mediana()  const {return galutinis_mediana_;}
     
-    void set_galutinis_vidurkis(double v) {galutinis_vidurkis_ = v;}
-    void set_galutinis_mediana(double v) {galutinis_mediana_ = v;}
     void setVardas(const string& v) {vardas_ = v;}
     void setPavarde(const string& p) {pavarde_ = p;}
     void setPazymiai(const vector<int>& p) {pazymiai_ = p;}
     void setEgzaminoPazymys(int e) {egzamino_pazymys_ = e;}
+    void apskaiciuotiGalutinius();
     
-    
-    double galutinis_balas(double (*) (const vector<int>&)) const;
     istream& readStudent(istream&, bool atsitiktiniai_balai = false);
+
+    ~Studentas() = default;
     };
 
 bool comparePagalMediana(const Studentas&, const Studentas&);
 bool comparePagalVidurki(const Studentas&, const Studentas&);
+bool comparePagalVarda(const Studentas&, const Studentas&);
 bool comparePagalPavarde(const Studentas&, const Studentas&);
-bool comparePagalEgzamina(const Studentas&, const Studentas&);
+
 
 
