7f9ba6f420d58a0c9e664b79b0690338e5183200
Emilija0621 <emilija.adomonyte@gmail.com>
2025-11-20 12:06:07 +0200

    Update studentas.h

--- Užduotis vektoriai (class)/studentas.h
+++ Užduotis vektoriai (class)/studentas.h
@@ -1,10 +1,15 @@
 #pragma once
 #include<vector>
 #include<string>
+#include<iostream>
+#include<algorithm>
 
 using std::string;
 using std::vector;
 using std::istream;
+using std::ostream;
+using std::endl;
+using std::swap;
 
 
 class Studentas{
@@ -19,11 +24,42 @@ private:
 public:
     Studentas() : egzamino_pazymys_(0), galutinis_vidurkis_(0), galutinis_mediana_(0){}
     Studentas(istream& is, bool atsitiktiniai_balai);
+    
+    //Kopijavimo konstruktorius
+    Studentas(const Studentas& stud)
+    : vardas_(stud.vardas_), pavarde_(stud.pavarde_), pazymiai_(stud.pazymiai_), egzamino_pazymys_(stud.egzamino_pazymys_), galutinis_vidurkis_(stud.galutinis_vidurkis_), galutinis_mediana_(stud.galutinis_mediana_) {}
+    
+    //Kopijavimo operatorius
+    Studentas& operator=(const Studentas& stud) {
+        if (this == &stud)
+            return *this;
+        
+        Studentas laikinas(stud);
+        swap(vardas_, laikinas.vardas_);
+        swap(pavarde_, laikinas.pavarde_);
+        swap(pazymiai_, laikinas.pazymiai_);
+        swap(egzamino_pazymys_, laikinas.egzamino_pazymys_);
+        swap(galutinis_vidurkis_, laikinas.galutinis_vidurkis_);
+        swap(galutinis_mediana_, laikinas.galutinis_mediana_);
+        return *this;
+    }
+    
+    //Destruktorius
+    ~Studentas(){
+        vardas_.clear();
+        pavarde_.clear();
+        pazymiai_.clear();
+        
+        egzamino_pazymys_ = 0;
+        galutinis_mediana_ = 0.0;
+        galutinis_vidurkis_ = 0.0;
+    }
+    
     inline string vardas() const {return vardas_;}
     inline string pavarde() const {return pavarde_;}
     inline double egzamino_pazymys() const {return egzamino_pazymys_;}
     inline double galutinis_vidurkis() const {return galutinis_vidurkis_;}
-    inline double galutinis_mediana()  const {return galutinis_mediana_;}
+    inline double galutinis_mediana() const {return galutinis_mediana_;}
     
     void setVardas(const string& v) {vardas_ = v;}
     void setPavarde(const string& p) {pavarde_ = p;}
@@ -32,14 +68,15 @@ public:
     void apskaiciuotiGalutinius();
     
     istream& readStudent(istream&, bool atsitiktiniai_balai = false);
-
-    ~Studentas() = default;
-    };
+    
+    //Isvesties operatorius
+    friend istream& operator>>(istream& is, Studentas& s);
+    //Ivesties operatorius
+    friend ostream& operator<<(ostream& os, const Studentas& s);
+    
+};
 
 bool comparePagalMediana(const Studentas&, const Studentas&);
 bool comparePagalVidurki(const Studentas&, const Studentas&);
 bool comparePagalVarda(const Studentas&, const Studentas&);
 bool comparePagalPavarde(const Studentas&, const Studentas&);
-
-
-
