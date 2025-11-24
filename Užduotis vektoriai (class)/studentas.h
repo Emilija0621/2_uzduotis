667fa651e9e43e3362add60e9abb9966489a1152
Emilija0621 <emilija.adomonyte@gmail.com>
2025-11-24 23:11:03 +0200

    Update studentas.h

--- Užduotis vektoriai (class)/studentas.h
+++ Užduotis vektoriai (class)/studentas.h
@@ -38,10 +38,10 @@ public:
     
     //Priskyrimo operatorius (copy assignment)
     Studentas& operator=(const Studentas& stud) {
+        Studentas laikinas(stud);
         if (this == &stud)
             return *this;
         
-        Studentas laikinas(stud);
         swap(vardas_, laikinas.vardas_);
         swap(pavarde_, laikinas.pavarde_);
         swap(pazymiai_, laikinas.pazymiai_);
