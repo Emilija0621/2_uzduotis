1a80be35f50fb16c4d76b3784af895ecb242a411
Emilija0621 <emilija.adomonyte@gmail.com>
2025-11-26 19:16:33 +0200

    Update studentas.h

--- Užduotis vektoriai (class)/studentas.h
+++ Užduotis vektoriai (class)/studentas.h
@@ -11,50 +11,45 @@ using std::ostream;
 using std::endl;
 using std::swap;
 
+#include "zmogus.h"
 
-class Studentas{
+
+class Studentas: public Zmogus {
 private:
-    string vardas_;
-    string pavarde_;
     vector<int> pazymiai_;
     int egzamino_pazymys_;
     double galutinis_vidurkis_;
     double galutinis_mediana_;
     
 public:
-    Studentas() : egzamino_pazymys_(0), galutinis_vidurkis_(0), galutinis_mediana_(0){}
+    Studentas() : Zmogus("", ""), egzamino_pazymys_(0), galutinis_vidurkis_(0), galutinis_mediana_(0){}
     Studentas(istream& is, bool atsitiktiniai_balai);
     
     //Kopijavimo konstruktorius (copy constructor)
     Studentas(const Studentas& stud)
-        : vardas_(stud.vardas_),
-          pavarde_(stud.pavarde_),
+        : Zmogus(stud.vardas_, stud.pavarde_),
           pazymiai_(stud.pazymiai_),
           egzamino_pazymys_(stud.egzamino_pazymys_),
           galutinis_vidurkis_(stud.galutinis_vidurkis_),
           galutinis_mediana_(stud.galutinis_mediana_)
     {}
-
     
     //Priskyrimo operatorius (copy assignment)
     Studentas& operator=(const Studentas& stud) {
-        Studentas laikinas(stud);
-        if (this == &stud)
-            return *this;
+        if (this == &stud) return *this;
         
-        swap(vardas_, laikinas.vardas_);
-        swap(pavarde_, laikinas.pavarde_);
-        swap(pazymiai_, laikinas.pazymiai_);
-        swap(egzamino_pazymys_, laikinas.egzamino_pazymys_);
-        swap(galutinis_vidurkis_, laikinas.galutinis_vidurkis_);
-        swap(galutinis_mediana_, laikinas.galutinis_mediana_);
+        vardas_ = stud.vardas_;
+        pavarde_ = stud.pavarde_;
+        pazymiai_ = stud.pazymiai_;
+        egzamino_pazymys_ = stud.egzamino_pazymys_;
+        galutinis_vidurkis_ = stud.galutinis_vidurkis_;
+        galutinis_mediana_ = stud.galutinis_mediana_;
+
         return *this;
     }
     
     //Destruktorius (destructor)
     ~Studentas(){
-        vardas_.clear();
-        pavarde_.clear();
         pazymiai_.clear();
         
         egzamino_pazymys_ = 0;
@@ -62,8 +57,19 @@ public:
         galutinis_vidurkis_ = 0.0;
     }
     
-    inline string vardas() const {return vardas_;}
-    inline string pavarde() const {return pavarde_;}
+    bool operator==(const Studentas& s) const {
+        return vardas_ == s.vardas_ &&
+               pavarde_ == s.pavarde_ &&
+               pazymiai_ == s.pazymiai_ &&
+               egzamino_pazymys_ == s.egzamino_pazymys_ &&
+               galutinis_vidurkis_ == s.galutinis_vidurkis_ &&
+               galutinis_mediana_ == s.galutinis_mediana_;
+    }
+
+    // Implementuoja abstrakčias Zmogus klasės funkcijas
+    string vardas() const override {return vardas_;}
+    string pavarde() const override {return pavarde_;}
+    
     inline double egzamino_pazymys() const {return egzamino_pazymys_;}
     inline double galutinis_vidurkis() const {return galutinis_vidurkis_;}
     inline double galutinis_mediana() const {return galutinis_mediana_;}
@@ -89,6 +95,3 @@ bool comparePagalVarda(const Studentas&, const Studentas&);
 bool comparePagalPavarde(const Studentas&, const Studentas&);
 
 void rule_of_three();
-
-
-
