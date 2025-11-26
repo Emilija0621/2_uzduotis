ed0cca7e19b405c1e6443404bdcbb8ab40e5db8b
Emilija0621 <emilija.adomonyte@gmail.com>
2025-11-26 19:16:04 +0200

    Update zmogus.h

--- Užduotis vektoriai (class)/zmogus.h
+++ Užduotis vektoriai (class)/zmogus.h
@@ -1,1 +1,25 @@
+#pragma once
+#include <string>
+
+using std::string;
+
+class Zmogus {
+protected:
+    string vardas_;
+    string pavarde_;
+
+public:
+    Zmogus() : vardas_(""), pavarde_("") {}
+    Zmogus(const string& v, const string& p) : vardas_(v), pavarde_(p) {}
+
+    virtual string vardas() const = 0;
+    virtual string pavarde() const = 0;
+
+    virtual ~Zmogus() {
+        vardas_.clear();
+        pavarde_.clear();
+    }
+};
+
+
 
