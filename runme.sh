f8e03daf9ce21f6e3bbcc6e10f1528c3bf3b67ca
Emilija0621 <emilija.adomonyte@gmail.com>
2025-12-04 15:25:29 +0200

    Update runme.sh

--- runme.sh
+++ runme.sh
@@ -3,4 +3,22 @@ mkdir -p build
 cd build
 cmake ..
 make
-./Studentai
+
+echo "Pasirinkite ką paleisti:"
+echo "1 - Programa"
+echo "2 - Unit testai"
+read -p "Įveskite 1 arba 2: " choice
+
+case $choice in
+    1)
+        echo "Paleidžiama programa"
+        ./studentas_programa
+        ;;
+    2)
+        echo "Paleidžiami unit testai"
+        ./unit_tests
+        ;;
+    *)
+        echo "Neteisingas pasirinkimas."
+        ;;
+esac
